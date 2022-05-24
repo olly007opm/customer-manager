fd1425413c84575077d28d07018b0ba2c91dfbf1
Olly <oliverpm7@gmail.com>
2022-05-24 14:59:53 +0100

    Update database URI

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ class Config:
     FLASK_ENV = environ.get('FLASK_ENV')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
